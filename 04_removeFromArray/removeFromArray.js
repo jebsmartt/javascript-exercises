cda828c7e5c081756813b66e9c2ed84b2208e608
jebsmartt <jeb.smartt@gmail.com>
2023-07-11 12:04:47 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...values) {
+    // get the array
+    let userArray = array
+    // remove items from the array
+    for (let i = 0; i < values.length; i++) {
+        userArray = userArray.filter((item) => item !== values[i])
+    }
+    // return the array
+    return userArray
 };
 
 // Do not edit below this line
