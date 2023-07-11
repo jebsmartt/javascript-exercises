fed7381c17ce976d5fb659d720483165384286ac
jebsmartt <jeb.smartt@gmail.com>
2023-07-11 11:20:41 -0700

    Complete execise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let characters = string.split("")
+    let newOrder = []
+    while (characters.length > 0) {
+        newOrder.push(characters.pop())
+    }
+    const reversedString = newOrder.join("")
+    return reversedString
 };
 
 // Do not edit below this line
