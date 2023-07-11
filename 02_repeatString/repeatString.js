8e4eca298700db43378aba03ed446c4d61c3a83c
jebsmartt <jeb.smartt@gmail.com>
2023-07-11 10:52:01 -0700

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0
+    let newString = ""
+    
+    if (num > -1) {
+        while (i < num) {
+            newString += string
+            i++
+        }
+        return newString
+    } else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
