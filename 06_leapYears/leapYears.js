e0c667a361ce4b8f8d6421b0ff885ea62a4ef5c0
jebsmartt <jeb.smartt@gmail.com>
2023-07-11 13:04:22 -0700

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    //safety checks
+    if (!Number.isInteger(year) || year < 0) return "ERROR";
+    // exception: years divisible by 100 unless also divisible by 400
+    if (year % 400 == 0) {
+        return true
+    } else if (year % 4 == 0 && year % 100 != 0) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
