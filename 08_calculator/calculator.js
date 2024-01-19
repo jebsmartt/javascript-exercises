80a10dd5052a3b2583b7ba57d8876f3e3a6f8938
jebsmartt <jeb.smartt@gmail.com>
2024-01-19 15:42:51 -0800

    Completed exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 0
+    );
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  return numbers.reduce(
+    (acc, current) => acc * current, 1
+  )
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x ** y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  const numbers = Array.from({ length: x }, (_, index) => index + 1)
+
+  return numbers.reduce(
+  (acc, current) => acc * current, 1
+  )
 };
 
 // Do not edit below this line
