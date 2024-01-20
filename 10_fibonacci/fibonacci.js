ae5ff0ca7aaa2cbb7834336c267075c9494a5fea
jebsmartt <jeb.smartt@gmail.com>
2024-01-19 16:51:28 -0800

    Completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let y
+    if (typeof index !== 'number') {
+        y = parseInt(index)
+    } else {
+        y = index
+    }
 
-};
+    if (y < 0) return "OOPS";
+    if (y == 0) return 0;
+    
+    let a = 0
+    let b = 1
+
+    for (let i=2; i <= y; i++) {
+        let current = b + a
+        a = b
+        b = current
+    }
+
+    return b
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
