a469c7376716d77357a69c33af41317f27b0da71
jebsmartt <jeb.smartt@gmail.com>
2024-01-19 19:56:29 -0800

    Completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // const titles = []
+    // books.forEach(book => {
+    //     titles.push(book.title)
+    // })
+    // return titles
+
+    return books.map((book) => book.title);
 
 };
 
