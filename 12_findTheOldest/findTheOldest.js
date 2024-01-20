0b7d49ca21f2810fdee252350e9f2358e804ff60
jebsmartt <jeb.smartt@gmail.com>
2024-01-19 20:29:09 -0800

    Completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce ((oldestPerson, currentPerson) => {
+        let calculatedAge = (currentPerson) => {
+            if (currentPerson.yearOfDeath === undefined) {
+                const currentYear = new Date().getFullYear();
+                return (currentYear - currentPerson.yearOfBirth)
+            } else {
+                return (currentPerson.yearOfDeath - currentPerson.yearOfBirth)
+            }
+        }
+        const oldestPersonAge = calculatedAge(oldestPerson)
+        const currentPersonAge = calculatedAge(currentPerson)
 
+        return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+    }, people[0])
 };
 
 // Do not edit below this line
