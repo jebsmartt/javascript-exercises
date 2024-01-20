2ab40db3da4c7006841c6dcb6755014d562db903
jebsmartt <jeb.smartt@gmail.com>
2024-01-19 16:19:57 -0800

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (aString) {
+    const filteredCharacters = aString.split('').filter(char => /[a-zA-Z0-9]/.test(char)).join('');
+    const reversedCharacters = [...filteredCharacters].reverse().join('')
+    return filteredCharacters === reversedCharacters
 };
+
+// const inputString = "Hello, World! 123";
+
+// const filteredCharacters = inputString.split('').filter(char => /[a-zA-Z0-9]/.test(char)).join('');
+
 
 // Do not edit below this line
 module.exports = palindromes;
